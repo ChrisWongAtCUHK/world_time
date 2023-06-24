4e82659a3e97c87342be4e4a4f5b060cdc933f3b
Chris Wong <wc38.wong@gmail.com>
2023-06-24 09:57:37 +0800

    Flutter Tutorial for Beginners #24 - Widget Lifecycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -10,11 +10,32 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function runs');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text('choose location screen'),
+    print('build function runs');
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 238, 238, 238),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 13, 71, 161),
+        title: const Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            count += 1;
+          });
+        },
+        child: Text('count is $count'),
       ),
     );
   }
